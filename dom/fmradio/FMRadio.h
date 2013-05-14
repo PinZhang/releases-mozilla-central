85f3be54226f0574ba7a11551d20760fee3623f0
pzhang <pzhang@mozilla.com>
2013-05-14 15:59:17 +0800

    Define WebFM API in webidl

---
+++ dom/fmradio/FMRadio.h
@@ -0,0 +1,74 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_fmradio_FMRadio_h
+#define mozilla_dom_fmradio_FMRadio_h
+
+#include "nsDOMEventTargetHelper.h"
+
+class nsPIDOMWindow;
+class nsIScriptContext;
+
+namespace mozilla {
+namespace dom {
+
+class DOMRequest;
+
+namespace fmradio {
+
+class FMRadio MOZ_FINAL : public nsDOMEventTargetHelper
+{
+public:
+  FMRadio();
+  ~FMRadio();
+
+  void Init(nsPIDOMWindow *aWindow);
+  void Shutdown();
+
+  /* WebIDL Interface */
+  nsPIDOMWindow * GetParentObject() const
+  {
+    return GetOwner();
+  }
+
+  virtual JSObject* WrapObject(JSContext* aCx,
+                               JS::Handle<JSObject*> aScope) MOZ_OVERRIDE;
+
+  bool Enabled() const;
+
+  bool AntennaAvailable() const;
+
+  Nullable<double> GetFrequency() const;
+
+  double FrequencyUpperBound() const;
+
+  double FrequencyLowerBound() const;
+
+  double ChannelWidth() const;
+
+  already_AddRefed<DOMRequest> Enable();
+
+  already_AddRefed<DOMRequest> Disable();
+
+  already_AddRefed<DOMRequest> SetFrequency(double frequency);
+
+  already_AddRefed<DOMRequest> SeekUp();
+
+  already_AddRefed<DOMRequest> SeekDown();
+
+  already_AddRefed<DOMRequest> CancelSeek();
+
+  IMPL_EVENT_HANDLER(enabled);
+  IMPL_EVENT_HANDLER(disabled);
+  IMPL_EVENT_HANDLER(antennaavailablechange);
+  IMPL_EVENT_HANDLER(frequencychange);
+};
+
+} // namespace fmradio
+} // namespace dom
+} // namespace mozilla
+
+#endif // mozilla_dom_fmradio_FMRadio_h
+
