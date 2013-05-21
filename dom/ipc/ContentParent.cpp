43803212454f3bc045e8ea507f1afe04c7798fbb
pzhang <pzhang@mozilla.com>
2013-05-21 12:41:32 +0800

    Fix crash bug
    FMRadioRequestParent is hold in CancelableRunnable, we can not delete
    it when DeallocPFMRadioRequest

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -2305,7 +2305,6 @@ ContentParent::AllocPFMRadioRequest(const FMRadioRequestParams& aParams)
     if (!AssertAppProcessPermission(this, "fmradio")) {
         return nullptr;
     }
-
     nsRefPtr<FMRadioRequestParent> result = new FMRadioRequestParent(aParams);
     result->Dispatch();
     return result.forget().get();
@@ -2319,7 +2318,8 @@ bool
 ContentParent::DeallocPFMRadioRequest(PFMRadioRequestParent* aActor)
 {
 #ifdef MOZ_B2G_FM
-    delete aActor;
+    FMRadioRequestParent* parent = static_cast<FMRadioRequestParent*>(aActor);
+    NS_RELEASE(parent);
     return true;
 #else
      MOZ_NOT_REACHED("No support for FMRadio on this platform!");
