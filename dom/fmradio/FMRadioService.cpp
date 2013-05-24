aeb72bab6bd51ac3d22994d3af2458bb3308cf1c
pzhang <pzhang@mozilla.com>
2013-05-24 12:48:18 +0800

    Sane Enable/Disable/SetFrequency

--- dom/fmradio/FMRadioService.cpp
+++ dom/fmradio/FMRadioService.cpp
@@ -16,7 +16,7 @@
 #include "mozilla/dom/fmradio/FMRadioChildService.h"
 
 #undef LOG
-#define LOG(args...) FM_LOG("PFMRadioService", args)
+#define LOG(args...) FM_LOG("FMRadioService", args)
 
 using namespace mozilla::hal;
 using mozilla::Preferences;
@@ -28,7 +28,7 @@ FMRadioService* gFMRadioService;
 FMRadioEventObserverList* gEventObserverList;
 
 FMRadioService::FMRadioService()
-  : mFrequency(0)
+  : mFrequencyInKHz(0)
 {
   LOG("constructor");
 
@@ -36,7 +36,7 @@ FMRadioService::FMRadioService()
   mEnabled = IsFMRadioOn();
   if (mEnabled)
   {
-    mFrequency = GetFMRadioFrequency();
+    mFrequencyInKHz = GetFMRadioFrequency();
   }
 
   RegisterFMRadioObserver(this);
@@ -48,6 +48,34 @@ FMRadioService::~FMRadioService()
 
   gFMRadioService = nullptr;
   gEventObserverList = nullptr;
+}
+
+/**
+ * Round the frequency to match the range of frequency and the channel width.
+ * If the given frequency is out of range, return 0.
+ * For example:
+ *  - lower: 87.5MHz, upper: 108MHz, channel width: 0.2MHz
+ *    87600 is rounded to 87700
+ *    87580 is rounded to 87500
+ *    109000 is not rounded, null will be returned
+ */
+int32_t
+RoundFrequncy(int32_t aFrequencyInKHz)
+{
+  // TODO read from settings
+  int32_t upper = 108000;
+  int32_t lower = 87500;
+  int32_t channelWidth = 100;
+
+  if (aFrequencyInKHz < lower ||
+      aFrequencyInKHz > upper) {
+    return 0;
+  }
+
+  int32_t partToBeRounded = aFrequencyInKHz - lower;
+  int32_t roundedPart = round(partToBeRounded / channelWidth) * channelWidth;
+
+  return lower + roundedPart;
 }
 
 void
@@ -70,20 +98,30 @@ FMRadioService::UnregisterHandler(FMRadioEventObserver* aHandler)
   }
 }
 
-
 void
-FMRadioService::Enable(double aFrequency, ReplyRunnable* aRunnable)
+FMRadioService::Enable(double aFrequencyInMHz, ReplyRunnable* aRunnable)
 {
   // We need to call EnableFMRadio() in main thread
   NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
 
   bool isEnabled = IsFMRadioOn();
   if (isEnabled) {
-    nsString error;
     aRunnable->SetReply(ErrorResponse(NS_ConvertASCIItoUTF16("It's enabled")));
     NS_DispatchToMainThread(aRunnable);
     return;
   }
+
+  int32_t roundedFrequency = RoundFrequncy(aFrequencyInMHz * 1000);
+  if (!roundedFrequency)
+  {
+    aRunnable->SetReply(ErrorResponse(
+      NS_ConvertASCIItoUTF16("Frequency is out of range")));
+    NS_DispatchToMainThread(aRunnable);
+    return;
+  }
+
+  // Cache the frequency value, and set it after the FM HW is enabled
+  mFrequencyInKHz = roundedFrequency;
 
   // TODO read from SettingsAPI
   FMRadioSettings info;
@@ -136,7 +174,7 @@ FMRadioService::Disable(ReplyRunnable* aRunnable)
 }
 
 void
-FMRadioService::SetFrequency(double aFrequency, ReplyRunnable* aRunnable)
+FMRadioService::SetFrequency(double aFrequencyInMHz, ReplyRunnable* aRunnable)
 {
   LOG("Check Main Thread");
   NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
@@ -150,7 +188,18 @@ FMRadioService::SetFrequency(double aFrequency, ReplyRunnable* aRunnable)
     return;
   }
 
-  SetFMRadioFrequency(aFrequency);
+  int32_t roundedFrequency = RoundFrequncy(aFrequencyInMHz * 1000);
+  if (!roundedFrequency)
+  {
+    aRunnable->SetReply(ErrorResponse(
+      NS_ConvertASCIItoUTF16("Frequency is out of range")));
+    NS_DispatchToMainThread(aRunnable);
+    return;
+  }
+
+  SetFMRadioFrequency(roundedFrequency);
+
+  UpdateFrequency();
 
   aRunnable->SetReply(SuccessResponse());
   NS_DispatchToMainThread(aRunnable);
@@ -174,9 +223,26 @@ FMRadioService::Notify(const FMRadioOperationInformation& info)
   switch (info.operation())
   {
     case FM_RADIO_OPERATION_ENABLE:
+    {
       LOG("FM HW is enabled.");
+
+      // To make sure the FM app will get right frequency after the FM
+      // radio is enabled, we have to set the frequency first.
+      SetFMRadioFrequency(mFrequencyInKHz);
+
+      // Update the current frequency without sending 'frequencyChange'
+      // msg, to make sure the FM app will get the right frequency when the
+      // 'enabled' event is fired.
+      mFrequencyInKHz = GetFMRadioFrequency();
       UpdatePowerState();
+
+      // The frequency is changed from '0' to some number, so we should
+      // send the 'frequencyChange' message manually.
+      double frequencyInMHz = mFrequencyInKHz / 1000.0;
+      gEventObserverList->Broadcast(FrequencyChangedEvent(frequencyInMHz));
+
       break;
+    }
     case FM_RADIO_OPERATION_DISABLE:
       LOG("FM HW is disabled.");
       UpdatePowerState();
@@ -198,7 +264,10 @@ FMRadioService::UpdatePowerState()
   if (enabled != mEnabled)
   {
     mEnabled = enabled;
-    gEventObserverList->Broadcast(StateChangedEvent(enabled));
+    double frequencyInMHz = mFrequencyInKHz / 1000.0;
+    // To make sure the FM app will get the right frequency when the
+    // 'enabled' event is fired.
+    gEventObserverList->Broadcast(StateChangedEvent(enabled, frequencyInMHz));
   }
 }
 
@@ -206,11 +275,11 @@ void
 FMRadioService::UpdateFrequency()
 {
   int32_t frequency = GetFMRadioFrequency();
-  if (mFrequency != frequency)
+  if (mFrequencyInKHz != frequency)
   {
-    mFrequency = frequency;
-    // TODO round and keep decimal precise
-    gEventObserverList->Broadcast(FrequencyChangedEvent(frequency));
+    mFrequencyInKHz = frequency;
+    double frequencyInMHz = mFrequencyInKHz / 1000.0;
+    gEventObserverList->Broadcast(FrequencyChangedEvent(frequencyInMHz));
   }
 }
 
