74201ad9aeabdec7d4e9e0187d494840f63e723a
pzhang <pzhang@mozilla.com>
2013-08-27 20:57:39 +0800

    Bug 870676 - Part III: marionette tests

---
+++ dom/fmradio/test/marionette/test_bug876597.js
@@ -0,0 +1,88 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+MARIONETTE_TIMEOUT = 10000;
+
+SpecialPowers.addPermission("fmradio", true, document);
+SpecialPowers.addPermission("settings-read", true, document);
+SpecialPowers.addPermission("settings-write", true, document);
+
+let FMRadio = window.navigator.mozFMRadio;
+let mozSettings = window.navigator.mozSettings;
+let KEY = "ril.radio.disabled";
+
+function verifyInitialState() {
+  log("Verifying initial state.");
+  ok(FMRadio, "FMRadio");
+  is(FMRadio.enabled, false, "FMRadio.enabled");
+  ok(mozSettings, "mozSettings");
+
+  checkRilSettings();
+}
+
+function checkRilSettings() {
+  log("Checking airplane mode settings");
+  let req = mozSettings.createLock().get(KEY);
+  req.onsuccess = function(event) {
+    ok(!req.result[KEY], "Airplane mode is disabled.");
+    enableFMRadio();
+  };
+
+  req.onerror = function() {
+    ok(false, "Error occurs when reading settings value.");
+    finish();
+  };
+}
+
+function enableFMRadio() {
+  log("Enable FM radio");
+  let req = FMRadio.enable(90);
+
+  req.onsuccess = function() {
+    enableAirplaneMode();
+  };
+
+  req.onerror = function() {
+    ok(false, "Failed to enable FM radio.");
+  };
+}
+
+function enableAirplaneMode() {
+  log("Enable airplane mode");
+  FMRadio.ondisabled = function() {
+    FMRadio.ondisabled = null;
+    enableFMRadioWithAirplanModeEnabled();
+  };
+
+  mozSettings.createLock().set({
+    "ril.radio.disabled": true
+  });
+}
+
+function enableFMRadioWithAirplanModeEnabled() {
+  log("Enable FM radio with airplane mode enabled");
+  let req = FMRadio.enable(90);
+  req.onerror = cleanUp();
+
+  req.onsuccess = function() {
+    ok(false, "FMRadio could be enabled when airplane mode is enabled.");
+  };
+}
+
+function cleanUp() {
+  let req = mozSettings.createLock().set({
+    "ril.radio.disabled": false
+  });
+
+  req.onsuccess = function() {
+    ok(!FMRadio.enabled, "FMRadio.enabled is false");
+    finish();
+  };
+
+  req.onerror = function() {
+    ok(false, "Error occurs when setting value");
+  };
+}
+
+verifyInitialState();
+
---
+++ dom/fmradio/test/marionette/test_set_frequency.js
@@ -0,0 +1,69 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+MARIONETTE_TIMEOUT = 10000;
+
+SpecialPowers.addPermission("fmradio", true, document);
+
+let FMRadio = window.navigator.mozFMRadio;
+
+function verifyInitialState() {
+  log("Verifying initial state.");
+  ok(FMRadio, "FMRadio");
+  is(FMRadio.enabled, false, "FMRadio.enabled");
+  setUp();
+}
+
+function setUp() {
+  FMRadio.enable(90);
+  FMRadio.onenabled = setFrequency;
+}
+
+function setFrequency() {
+  log("Set Frequency");
+  var request = FMRadio.setFrequency(100);
+  ok(request, "setFrequency request should not be null.");
+
+  request.onsuccess = setOutOfRangeFrequency;
+  request.onerror = function() {
+    ok(false, "setFrequency request should not fail.");
+  };
+}
+
+function setOutOfRangeFrequency() {
+  log("Set Frequency that out of the range");
+  var request = FMRadio.setFrequency(FMRadio.frequencyUpperBound + 1);
+  ok(request, "setFrequency should not be null.");
+
+  request.onsuccess = function() {
+    ok(false, "The request of setting an out-of-range frequency should fail.");
+  };
+  request.onerror = setFrequencyWhenSeeking;
+}
+
+function setFrequencyWhenSeeking() {
+  log("Set frequency when seeking");
+  var request = FMRadio.seekUp();
+  ok(request, "Seekup request should not be null.");
+
+  request.onsuccess = function() {
+    ok(false, "Seek request should fail.");
+  };
+
+  var setFreqRequest = FMRadio.setFrequency(101);
+  ok(setFreqRequest, "setFrequency request should not be null.");
+
+  setFreqRequest.onsuccess = cleanUp;
+}
+
+function cleanUp() {
+  FMRadio.disable();
+  FMRadio.ondisabled = function() {
+    FMRadio.ondisabled = null;
+    ok(!FMRadio.enabled, "FMRadio.enabled is false");
+    finish();
+  };
+}
+
+verifyInitialState();
+
